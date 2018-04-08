bf6508992bcac01d3937f575459cef94b7ece37b
VerntyChang <promise2005@gmail.com>
2018-04-09 00:00:35 +0800

    organize files

---
+++ TaipeiParks/Model/SpotTableViewCell.m
@@ -0,0 +1,14 @@
+//
+//  SpotTableViewCell.m
+//  TaipeiParks
+//
+//  Created by Vernty on 2018/4/5.
+//  Copyright © 2018年 VerntyChang. All rights reserved.
+//
+
+#import "SpotTableViewCell.h"
+
+@implementation SpotTableViewCell
+ 
+
+@end
